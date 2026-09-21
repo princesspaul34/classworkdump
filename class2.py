cdac227bcd2ac6e809263efba62ab156a36aa492
abdul0nkk <abdulazizhassannyako@gmail.com>
2026-09-21 11:50:35 +0100

    my first classwork

---
+++ class2.py
@@ -0,0 +1,51 @@
+# variable
+name = "john" 
+school = "AUN"
+age = 25
+score = 100
+height = 1.75
+weight = 72.6
+True
+False
+#names
+names = ["john", "jane", "bob"]
+#numbers
+numbers = (1, 2, 3, 4, 5)
+#dictionary
+school_dict = {"name": "AUN", "location": "Thailand", "students": 1000}
+#print
+print("Name:", name)
+print("School:", school)
+print("Age:", age)
+print("Score:", score)
+print("Height:", height)
+print("Weight:", weight)
+print("Names:", names)
+print("Numbers:", numbers)
+print("School Dictionary:", school_dict)
+#print second item
+print("school:", school)
+#print last item
+print("weight:", weight)
+#print data type
+print("Data type of name:", type(name))
+print("Data type of school:", type(school))
+print("Data type of age:", type(age))
+print("Data type of score:", type(score))
+print("Data type of height:", type(height))
+print("Data type of weight:", type(weight))
+print("Data type of names:", type(names))
+print("Data type of numbers:", type(numbers))
+print("Data type of school_dict:", type(school_dict))
+#write a code to add, multiply, divide, and subtract two numbers
+x = 6
+y = 9
+print("Multiplication:", x * y)
+print("Division:", x / y)
+print("Subtraction:", x - y)
+#write a code to perform the following operations: x**2+y%z*3, (x+y)//z, x/y+x//y, -x**y
+z = 3
+print("Operation 1:", x**2 + y%z*3)
+print("Operation 2:", (x+y)//z)
+print("Operation 3:", x/y + x//y)
+print("Operation 4:", -x**y)
